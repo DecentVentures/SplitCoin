4a3c3650b2c79f16e65516f819f6bc98a95a6b87
Micah Riggan <micahriggan@gmail.com>
2017-12-26 14:55:54 -0600

    Deployed new version of splitcoin factory

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,12 @@
 module.exports = {
-  ropsten: { active: "0x3c674646ebe5c97d004d50d4b22ca34b2b9fe4ed", deprecated: [] },
-  main: {active: "0xFBD6AF12f2C88fdc69FEe2fe0e161d0edf63926F", deprecated: []}
+  ropsten: {
+    active: "0x3c674646ebe5c97d004d50d4b22ca34b2b9fe4ed",
+    deprecated: []
+  },
+  main: {
+    active: "0x4A53e0f5144aDcBB606087615bf78078C2097F02",
+    deprecated: [
+      "0x4A53e0f5144aDcBB606087615bf78078C2097F02"
+    ]
+  }
 };
