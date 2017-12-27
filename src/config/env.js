dc0c4acdcb5e638660b6c33b68f4801d1f831938
Micah Riggan <micahriggan@gmail.com>
2017-12-26 22:43:00 -0600

    updating main contract to newly deployed contract

--- src/config/env.js
+++ src/config/env.js
@@ -4,8 +4,9 @@ module.exports = {
     deprecated: []
   },
   main: {
-    active: "0x50D998D0fAF680B9cf44D6CFd9f7c3654056dCc5",
+    active: "0x2B96Efc474B9FB935951cA65F10d0cEcDc480D57",
     deprecated: [
+      "0x50D998D0fAF680B9cf44D6CFd9f7c3654056dCc5",
       "0x4A53e0f5144aDcBB606087615bf78078C2097F02"
     ]
   }
